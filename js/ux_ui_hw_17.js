4304e8e139267e4711e1ef230f94db749044e74a
SeulgiH15 <93741167+SeulgiH15@users.noreply.github.com>
2022-06-01 23:54:10 -0400

    c1

---
+++ js/ux_ui_hw_17.js
@@ -0,0 +1,47 @@
+$("#about").hover(function(){
+    $("#about").css("text-decoration", "underline");
+    $("#about").css("color", "#2701FE");
+}, function(){
+    $("#about").css("text-decoration", "none");
+    $("#about").css("color", "#5C1F63");
+})
+
+$("#work").hover(function(){
+    $("#work").css("text-decoration", "underline");
+    $("#work").css("color", "#2701FE");
+}, function(){    
+    $("#work").css("text-decoration", "none");
+    $("#work").css("color", "#5C1F63");
+})
+
+$("#resume").hover(function(){
+    $("#resume").css("text-decoration", "underline");
+    $("#resume").css("color", "#2701FE");
+}, function(){      
+    $("#resume").css("text-decoration", "none");
+    $("#resume").css("color", "#5C1F63");
+})
+
+$(".csbutton1").hover(function(){
+    $(".csbutton1").css("text-decoration", "underline");
+    $(".csbutton1").css("color", "white");
+}, function(){    
+    $(".csbutton1").css("text-decoration", "none");
+    $(".csbutton1").css("color", "white");
+})
+
+$(".csbutton2").hover(function(){
+    $(".csbutton2").css("text-decoration", "underline");
+    $(".csbutton2").css("color", "white");
+}, function(){    
+    $(".csbutton2").css("text-decoration", "none");
+    $(".csbutton2").css("color", "white");
+})
+
+$(".csbutton3").hover(function(){
+    $(".csbutton3").css("text-decoration", "underline");
+    $(".csbutton3").css("color", "white");
+}, function(){    
+    $(".csbutton3").css("text-decoration", "none");
+    $(".csbutton3").css("color", "white");
+})
---
+++ js/ux_ui_hw_20_casestudycra.js
@@ -0,0 +1,31 @@
+$("#about").hover(function(){
+    $("#about").css("text-decoration", "underline");
+    $("#about").css("color", "#2701FE");
+}, function(){
+    $("#about").css("text-decoration", "none");
+    $("#about").css("color", "#5C1F63");
+})
+
+$("#work").hover(function(){
+    $("#work").css("text-decoration", "underline");
+    $("#work").css("color", "#2701FE");
+}, function(){    
+    $("#work").css("text-decoration", "none");
+    $("#work").css("color", "#5C1F63");
+})
+
+$("#resume").hover(function(){
+    $("#resume").css("text-decoration", "underline");
+    $("#resume").css("color", "#2701FE");
+}, function(){      
+    $("#resume").css("text-decoration", "none");
+    $("#resume").css("color", "#5C1F63");
+})
+
+$(".footercontact").hover(function(){
+    $(".footercontact").css("text-decoration", "underline");
+    $(".footercontact").css("color", "#2701FE");
+}, function(){    
+    $(".footercontact").css("text-decoration", "none");
+    $(".footercontact").css("color", "#5C1F63");
+})
